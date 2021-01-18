9806f2129a09f48e69416050686efa557bb99e68
m1lovanovt <timcess@gmail.com>
2021-01-18 13:17:22 +0700

    Added comments to build scripts

--- build.bat
+++ build.bat
@@ -1,6 +1,13 @@
 @echo off
 
 echo [.] Building
+rem --clean   Clean PyInstaller cache and remove temporary files before building.
+rem --onefile Create a one-file bundled executable
+rem --name    Name of the executable
+rem --add-data Add data to executable. Format:
+rem 		--add-data="SRC;DST" on Windows
+rem 		--add-data="SRC:DST" on Linux
+rem 		DST is a path relative to executable file, when running. To this pass data will be unpacked
 pyinstaller --log-level=WARN --clean ^
 	--onefile --noupx --name wisecreator ^
 	--add-data="wisecreator/data;./data/" ^
